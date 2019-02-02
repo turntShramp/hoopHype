65d42f2280a137af35681d383b07f9f174a661c3
shramp <seamusmurchadh@gmail.com>
2019-02-02 17:44:48 -0500

    logo is now a home button

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -3,6 +3,11 @@ $("#teamPage").hide();
 $("#gamePage").hide();
 
 $(document).ready(function() {
+    $(".hypeLOGO").on('click', function() {
+        $("#landingPage").hide();
+        $("#teamPage").show();
+        $("#gamePage").hide();
+    })
 
     $("#start").on('click', function() {
         $("#landingPage").hide();
